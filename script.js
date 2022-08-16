ee375638610f8c76f9bb421b48b178c75e0ec116
israelsanttana <israelsantanasantos7@gmail.com>
2022-08-16 13:41:05 -0300

    minutos e segundos configurados

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 let music = document.querySelector('audio');
 
+let musicDuration = document.querySelector('.final');
+let wallpaper = document.querySelector('.front-cover');
+let musicName = document.querySelector('.name-song h2');
+let artistName = document.querySelector('.artist p');
+
+musicDuration.textContent = secondsToMinutes (Math.floor(music.duration));
+
 document.querySelector('#play').addEventListener('click', playerMusic);
 document.querySelector('#pause').addEventListener('click', pauseMusic);
 music.addEventListener('timeupdate', barUpdate);
@@ -21,4 +28,17 @@ function pauseMusic(){
 function barUpdate(){
     let bar = document.querySelector('progress');
     bar.style.width = Math.floor((music.currentTime / music.duration) * 100) + '%';
+    let elapsed = document.querySelector('.initial');
+    elapsed.textContent = secondsToMinutes (Math.floor(music.currentTime));
+}
+
+function secondsToMinutes(seconds){
+    let minutesField = Math.floor(seconds / 60);
+    let secondsField = seconds % 60;
+    if(secondsField < 10){
+        secondsField = '0' + secondsField
+    }
+
+    return minutesField+ ':' + secondsField;
+
 }
